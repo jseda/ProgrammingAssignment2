bee81fc64f3b451d34314e4cbc26c2f3b24b311e
Jan Šeda <jseda@users.noreply.github.com>
2015-10-25 23:27:42 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -2,21 +2,24 @@
 ## In case there is the matrix already calculated the cached value is used instead of calculation
 makeCacheMatrix <- function(x = matrix()) {
 
-	## Initialization
+	# Initialization of cache to null
         invValX <- NULL
         
+        # Store matrix
         set <- function(Y)
         {
 	        x <<- y
 	        invValX <<- NULL
         }
         
+        # Get matrix
         get <- function() x
         
         # Calculate the inverse value
         setinverse<- function(inverse) invValX <<-inverse
         getinverse <- function() invValX
         
+        # Return a list object
         list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
@@ -24,19 +27,20 @@ makeCacheMatrix <- function(x = matrix()) {
 ## If the object is not stored in cached it is created and set to cached and retuned to calling context.
 cacheSolve <- function(x, ...) 
 {
-	## Get the inverse value of 'x' if any
+	# Get the inverse value of 'x' if any
 	invValX <- x$getinverse()
     
-	## is there any object in cache?
+	# is there any object in cache?
 	if (!is.null(invValX))
 	{
-		message("The cached value is returned.")
+		message("The cached inverse value is returned.")
         	return(invValX)
 	}
 	
-	## Cache is empty, calculate the value and store it in cache
+	# Cache is empty, calculate the value and store it in cache
+	message("Calculating a new inverse value and storing it in cache.")
 	invValX <- solve(x$get())
-        #set the object into cache
+        # Set the object into cache
         x$setinverse(invValX)
     
 	return(invValX)
